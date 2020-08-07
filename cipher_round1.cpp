930662830f6a8eb7913ccb0f6cd5cfc185c4d5bf
shivamtripathi91 <65054486+shivamtripathi91@users.noreply.github.com>
2020-08-08 02:28:18 +0530

    Update cipher_round1.cpp

--- cipher_round1.cpp
+++ cipher_round1.cpp
@@ -1,5 +1,33 @@
 /*
-Shivam Tripathi
+SHIVAM TRIPATHI
+
+The subject you're submitting assignment for: Data Structure
+
+Q: A bracket is considered to be following of the characters ‘(‘, ‘)’, ‘[‘, ‘]’, ‘{], ‘}’.
+Two brackets are considered to be a matched pair if the an opening bracket
+( i.e., (, [, or { ) occurs to the left of a closing bracket (i.e., ), ], or }) of the exact same
+type. There are three types of matched pairs of brackets: [ ], { }, and ( ).
+A matching pair of brackets is not balanced if the set of brackets it encloses are not
+matched.
+For example, { [ ( ] ) } is not balanced because the contents in
+between { and } are not balanced. The pair of square brackets encloses a single,
+unbalanced opening bracket, (, and the pair of parentheses encloses a single,
+unbalanced closing square bracket, ].
+By this logic, we say a sequence of brackets is balanced if the following conditions
+are met:
+1. It contains no unmatched brackets.
+2. The subset of brackets enclosed within the confines of a matched pair of brackets
+is also a matched pair of brackets.
+Given n string of rackets, determine whether each sequence of brackets is
+balanced. If string is balanced, return YES, otherwise return NO.
+EXAMPLE:
+Input:
+2
+[ ( { } ) ]
+{ ( ] }
+Output:
+YES
+NO
 */
 
 #include<bits/stdc++.h>
@@ -37,6 +65,7 @@ void isBalanced(string str)
 
 int main()
 {
+//un-comment the following for input-output in text files(offline mode)
 // #ifndef ONLINE_JUDGE
 //     freopen("input.txt", "r", stdin);
 //     freopen("output.txt", "w", stdout);
